f3879b8034ace3f2314f23a425248aade93ee02a
MaxxBravo <maxxbr.iee@gmail.com>
2019-09-18 12:11:05 -0300

    jquery implementation

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 const fs = require('fs');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
@@ -16,6 +21,16 @@ app.get('/insertar', (req, res) => {
 app.get('/datos', (req, res) => {
     res.render('datos.html');
 });
+
+app.post('/recibirDatos', (req, res) => {
+    let data = req.body;
+
+    console.log(data);
+
+    res.send(true);
+});
+
+
 
 // app.get('/insertar/:mensaje', (req, res) => {
 //     res.render('main.html',{mensaje:req.params.mensaje});
