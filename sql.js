818bb4e0bfa9ee2c6d58bcc22d45ec77c6fba5a9
MaxxBravo <maxxbr.iee@gmail.com>
2019-09-25 10:25:56 -0300

    Add sql module para docker

--- sql.js
+++ sql.js
@@ -1,44 +1,41 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '10.10.20.65',
+    port: 4003,
+    user: 'root',
+    password: '1234',
+    database: 'gst'
+});
 
-/**
- * TODO(developer): specify SQL connection details
- */
-const connectionName =
-     '35.198.5.54'; //process.env.INSTANCE_CONNECTION_NAME ||
-const dbUser = process.env.SQL_USER || 'mbravo';
-const dbPassword = process.env.SQL_PASSWORD || 'capacitacion';
-const dbName = process.env.SQL_NAME || 'webcapdb';
-
-const mysqlConfig = {
-    connectionLimit: 1,
-    user: dbUser,
-    password: dbPassword,
-    database: dbName,
-};
-if (process.env.NODE_ENV === 'production') {
-    mysqlConfig.socketPath = `/cloudsql/${connectionName}`;
-}
-
-// Connection pools reuse connections between invocations,
-// and handle dropped or expired connections automatically.
-let mysqlPool;
-
-function consult(query, callback) {
-    if (!mysqlPool) {
-        mysqlPool = mysql.createPool(mysqlConfig);
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
     }
+    console.log('connected as id ' + connection.threadId);
+});
 
-    mysqlPool.query(query, (err, results) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(JSON.stringify(results));
-            callback(results);
-        }
-    });
-}
 
-module.exports = {
-    consult
-}
-// consult();
+
+// Usa minúsculas para los nombres de los objetos
+//  Table gst.entidades
+//      id int
+//      nombre varchar
+//      apellido varchar
+
+
+/** Tarea
+ * 
+ * Pasar el codigo de abajo a una funciones que te permitan ejecutar queries de SELECT, INSERT, UPDATE y DELETE
+ * Exportar las funciones para dejarlas disponibles en el main.js y pasar las operaciones realizadas
+ * para guardar los datos ingresados en la variable a guardarlos en la base de datos MYSQL
+ * 
+ */
+
+let query = 'select * from gst.entidades;';
+
+connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+
+    console.log(results);
+});
